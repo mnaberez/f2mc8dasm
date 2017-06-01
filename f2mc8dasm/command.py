ed7a48172e42e8ea0889b4bc9584de1d2f71ecf6
Mike Naberezny <mike@naberezny.com>
2017-06-01 09:20:58 -0700

    Temporarily add jump tables for Clarion PU-1666A main MCU

--- f2mc8dasm/command.py
+++ f2mc8dasm/command.py
@@ -4,7 +4,7 @@ from f2mc8dasm.disasm import disassemble_inst
 from f2mc8dasm.trace import Tracer
 from f2mc8dasm.memory import Memory
 from f2mc8dasm.listing import Printer
-from f2mc8dasm.symbols import MB89620R_SYMBOLS
+from f2mc8dasm.symbols import MB89670_SYMBOLS
 
 def main():
     with open(sys.argv[1], 'rb') as f:
@@ -22,15 +22,60 @@ def main():
         0xffe0, 0xffe2, 0xffe4, 0xffe6, 0xffe8, 0xffea, 0xffec, 0xffee,
         0xfff0, 0xfff2, 0xfff4, 0xfff6, 0xfff8, 0xfffa,
         # reset
-        0xfffe
+        0xfffe,
     ]
+
+    # XXX put this in a more sensible place
+    vectors += [
+        # pu-1666a main mcu indirect jmp table at 0xefbb
+        0xefbd,0xefc2,0xefc7,0xefcc,0xefd1,0xefd6,
+    ]
+    def jump_table(start_address, length):
+        address = start_address
+        for i in range(length):
+            vectors.append(address)
+            address += 2
+    # pu-1666a main mcu jump tables
+    jump_table(0x81a8, 6)
+    jump_table(0xf139, 6)
+    jump_table(0xd7e1, 16)
+    jump_table(0xde79, 9)
+    jump_table(0xedd8, 4)
+    jump_table(0xee39, 7)
+    jump_table(0xee77, 6)
+    jump_table(0xf254, 9)
+    jump_table(0xd501, 16)
+    jump_table(0xd528, 16)
+    jump_table(0x8f44, 14)
+    jump_table(0x958a, 51)
+    jump_table(0x9a51, 33)
+    jump_table(0x9ee0, 11)
+    jump_table(0xa4a0, 11)
+    jump_table(0xac03, 17)
+    jump_table(0xae73, 8)
+    jump_table(0xb318, 17)
+    jump_table(0xb467, 7)
+    jump_table(0xb5a4, 7)
+    jump_table(0xbe62, 8)
+    jump_table(0xbe62, 6)
+    jump_table(0xc354, 11)
+    jump_table(0xc4c9, 10)
+    jump_table(0xc9ec, 6)
+    jump_table(0xca39, 6)
+    jump_table(0xcb16, 6)
+    jump_table(0xe223, 7)
+    jump_table(0xe3bd, 14)
+    jump_table(0xee20, 3)
+    jump_table(0xee5b, 6)
+    jump_table(0xf4b7, 6)
+    jump_table(0xfe00, 66)
 
     traceable_range = range(start_address, start_address + len(rom) + 1)
     tracer = Tracer(memory, entry_points, vectors, traceable_range)
     tracer.trace(disassemble_inst)
 
     # XXX move symbol generation to a more sensible place
-    symbols = MB89620R_SYMBOLS.copy()
+    symbols = MB89670_SYMBOLS.copy()
     for address in range(start_address, len(memory)):
         if memory.is_call_target(address):
             if memory.is_instruction_start(address):
