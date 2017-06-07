a591371c13c018353aff662d14a1272cabdae1e8
Mike Naberezny <mike@naberezny.com>
2017-06-07 09:36:54 -0700

    Trace instructions in order instead of randomly

--- f2mc8dasm/tests/test_trace.py
+++ f2mc8dasm/tests/test_trace.py
@@ -1,5 +1,5 @@
 import unittest
-from f2mc8dasm.trace import TraceQueue
+from f2mc8dasm.trace import TraceQueue, SortedSet
 
 class TraceQueueTests(unittest.TestCase):
     # __init__
@@ -7,7 +7,7 @@ class TraceQueueTests(unittest.TestCase):
     def test_ctor(self):
         queue = TraceQueue()
         self.assertEqual(queue.traced_addresses, set())
-        self.assertEqual(queue.untraced_addresses, set())
+        self.assertEqual(queue.untraced_addresses, SortedSet())
 
     # __len__
 
@@ -26,13 +26,20 @@ class TraceQueueTests(unittest.TestCase):
         queue.push(0x0006)
         self.assertEqual(queue.untraced_addresses, set([0x0005, 0x0006]))
 
-    def test_push_doesnt_add_address_if_already_traced(self):
+    def test_push_doesnt_add_address_if_already_in_traced(self):
         queue = TraceQueue()
         queue.push(0x0005)
         self.assertEqual(len(queue), 1)
         queue.pop()
         queue.push(0x0005)
         self.assertEqual(len(queue), 0)
+
+    def test_push_doesnt_add_address_if_already_in_untraced(self):
+        queue = TraceQueue()
+        queue.push(0x0005)
+        self.assertEqual(len(queue), 1)
+        queue.push(0x0005)
+        self.assertEqual(len(queue), 1)
 
     # pop
 
@@ -44,7 +51,7 @@ class TraceQueueTests(unittest.TestCase):
         except KeyError as exc:
             self.assertEqual(exc.args[0], 'pop from empty trace queue')
 
-    def test_pop_removes_from_untraced_and_adds_to_traced(self):
+    def test_pop_removes_address_from_untraced_and_adds_to_traced(self):
         queue = TraceQueue()
         queue.push(0x0005)
         self.assertEqual(queue.untraced_addresses, set([0x0005]))
@@ -52,3 +59,102 @@ class TraceQueueTests(unittest.TestCase):
         self.assertEqual(address, 0x0005)
         self.assertEqual(queue.untraced_addresses, set())
         self.assertEqual(queue.traced_addresses, set([0x0005]))
+
+    def test_pop_returns_addresses_in_sorted_order(self):
+        queue = TraceQueue()
+        queue.push(0x0003)
+        queue.push(0x0001)
+        queue.push(0x0002)
+        self.assertEqual(queue.pop(), 0x0001)
+        self.assertEqual(queue.pop(), 0x0002)
+        self.assertEqual(queue.pop(), 0x0003)
+
+
+class SortedSetTests(unittest.TestCase):
+    # __init__
+
+    def test_ctor(self):
+        s = SortedSet([1, 2])
+        self.assertTrue(1 in s)
+        self.assertTrue(2 in s)
+
+    # __len__
+
+    def test_len_initially_0(self):
+        s = SortedSet()
+        self.assertEqual(len(s), 0)
+
+    def test_len(self):
+        s = SortedSet([1, 2])
+        s.add(3)
+        self.assertEqual(len(s), 3)
+
+    # __contains__
+
+    def test_contains(self):
+        s = SortedSet([1])
+        s.add(2)
+        self.assertTrue(1 in s)
+        self.assertTrue(2 in s)
+        self.assertFalse(3 in s)
+
+    # __iter__
+
+    def test_iter(self):
+        s = SortedSet([2,1,3])
+        s.add(5)
+        s.add(4)
+        self.assertEqual(list(s), [1,2,3,4,5])
+
+    # __eq__
+
+    def test_eq_set(self):
+        s = SortedSet([1, 2])
+        self.assertTrue(s == set([1, 2]))
+        self.assertFalse(s == set([1, 2, 3]))
+
+    def test_eq_SortedSet(self):
+        s = SortedSet([1, 2])
+        self.assertTrue(s == SortedSet([1, 2]))
+        self.assertFalse(s == SortedSet([1, 2, 3]))
+
+    # add
+
+    def test_add_new_item(self):
+        s = SortedSet()
+        s.add(1)
+        self.assertEqual(s, set([1]))
+
+    def test_add_existing_item(self):
+        s = SortedSet()
+        s.add(1)
+        s.add(1)
+        s.add(1)
+        self.assertEqual(s, set([1]))
+        self.assertEqual(list(s), [1])
+
+    # remove
+
+    def test_remove(self):
+        s = SortedSet([1, 2, 3])
+        s.remove(2)
+        self.assertEqual(s, set([1, 3]))
+        self.assertEqual(list(s), [1, 3])
+
+    def test_remove_nonexistent_item(self):
+        s = SortedSet()
+        self.assertRaises(KeyError, s.remove, 1)
+
+    # pop
+
+    def test_pop(self):
+        s = SortedSet([3,1,4])
+        s.add(2)
+        self.assertEqual(s.pop(), 1)
+        self.assertEqual(s.pop(), 2)
+        self.assertEqual(s.pop(), 3)
+        self.assertEqual(s.pop(), 4)
+
+    def test_pop_empty(self):
+        s = SortedSet()
+        self.assertRaises(KeyError, s.pop)
--- f2mc8dasm/trace.py
+++ f2mc8dasm/trace.py
@@ -73,8 +73,12 @@ class Tracer(object):
 
 
 class TraceQueue(object):
+    '''A queue for holding addresses that need to be traced.  Addresses may be
+    pushed in any order but are always popped in sorted order.  An address that
+    was already pushed will be ignored, even if it was popped.'''
+
     def __init__(self):
-        self.untraced_addresses = set()
+        self.untraced_addresses = SortedSet()
         self.traced_addresses = set()
 
     def __len__(self):
@@ -82,7 +86,8 @@ class TraceQueue(object):
 
     def push(self, address):
         if address not in self.traced_addresses:
-            self.untraced_addresses.add(address)
+            if address not in self.untraced_addresses:
+                self.untraced_addresses.add(address)
 
     def pop(self):
         if self.untraced_addresses:
@@ -90,3 +95,46 @@ class TraceQueue(object):
             self.traced_addresses.add(address)
             return address
         raise KeyError("pop from empty trace queue")
+
+
+class SortedSet(object):
+    '''A set-like object where pop() returns items in sorted order'''
+
+    def __init__(self, items=None):
+        self.set_of_items = set()  # for presence
+        self.list_of_items = []    # for ordered retrieval
+        if items is not None:
+            for item in items:
+                self.add(item)
+
+    def __len__(self):
+        return len(self.set_of_items)
+
+    def __contains__(self, item):
+        return item in self.set_of_items
+
+    def __iter__(self):
+        return iter(self.list_of_items)
+
+    def __eq__(self, other):
+        return other == self.set_of_items
+
+    def add(self, item):
+        if item not in self.set_of_items:
+            self.set_of_items.add(item)
+            self.list_of_items.append(item)
+            self.list_of_items.sort()
+
+    def remove(self, item):
+        if item not in self.set_of_items:
+            raise KeyError(item)
+        self.set_of_items.remove(item)
+        self.list_of_items.remove(item)
+
+    def pop(self):
+        if self.set_of_items:
+            item = self.list_of_items.pop(0)
+            self.set_of_items.remove(item)
+            return item
+        else:
+            raise KeyError("pop from empty SortedSet")
